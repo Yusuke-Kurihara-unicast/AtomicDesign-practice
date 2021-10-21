739628e93c931248cf450fd4befa9f96b2c49d7f
Yusuke-Kurihara-unicast <91720261+Yusuke-Kurihara-unicast@users.noreply.github.com>
2021-10-21 12:19:27 +0900

    organismまで

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
+import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
+import { SearchInput } from "./components/molecule/SearchInput";
+import { UserCard } from "./components/organisms/user/UserCard";
 import "./styles.css";
+
+const user = {
+  image: "https://source.unsplash.com/ZlFKIG6dApg",
+  name: "康夫",
+  email: "1111@example.com",
+  phone: "ccc-cccc-aaaa",
+  company: {
+    name: "康夫の倉庫",
+    website: "https://www.google.com/"
+  }
+};
 
 export default function App() {
   return (
     <div className="App">
       <PrimaryButton>aaaa</PrimaryButton>
+      <SecondaryButton>bbb</SecondaryButton>
+      <br />
+      <SearchInput />
+      <UserCard user={user} />
     </div>
   );
 }
